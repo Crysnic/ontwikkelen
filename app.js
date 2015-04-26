ceea9838227c2e359a88d942ae8b1a680340454a
Crysnic <vovarx@gmail.com>
2015-04-26 21:23:52 +0300

    Add a test page for training

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var bodyParser = require('body-parser');
 var path = require("path");
 var favicon = require('serve-favicon');
 
-var routes = require("./routes/index");
+var test = require("./routes/index");
 var mainRoute = require("./routes/ontwikkelen_router")
 
 var app = express();
@@ -15,7 +15,7 @@ app.set('view engine', 'jade');
 app.use(bodyParser.json());
 app.use( favicon(path.join(__dirname, 'public', 'favicon.ico')) );
 // test jane
-app.use('/test', routes);
+app.use('/test', test);
 app.use('/', mainRoute);
 
 
--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,11 @@ var router = express.Router();
 /* GET test page. */
 router.get('/', function(req, res, next) {
   res.render('test', {
-                       name: 'Владимир',
-                       surname: 'Удалых'
+    name: 'Владимир',
+    surname: 'Удалых',
+    birthdate: '11-11-1992',
+    city: 'Днепропетровск',
+    email: 'vovarx@gmail.com'
   });
 });
 
