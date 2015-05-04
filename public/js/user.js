ef72efbc4e033f7b6ded0a957a91d1997636c5da
Crysnic <vovarx@gmail.com>
2015-05-04 22:15:22 +0300

    Add js file for a user page

--- public/js/user.js
+++ public/js/user.js
@@ -1,9 +1,9 @@
 window.addEventListener("load", function() {
     var form = document.forms[0];
-    var avatarButton = form.getElementsByClassName('avatar')[0];
+    var avatarButton = document.getElementsByClassName('avatar')[0];
     
     avatarButton.onclick = function(event) {
-        if (event.target.tagName == "IMG")
+        if (event.target.tagName === "IMG")
             form.classList.toggle('choosePicture');
-    }
+    };
 });
